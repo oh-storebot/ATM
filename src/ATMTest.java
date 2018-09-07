afe41f3e1b38adc1a8b69a9925bfd5cef4584248
oh-storebot <eric@negron.nu>
2018-09-07 12:47:52 -0400

    J Unit error corrections but not passing tests

--- src/ATMTest.java
+++ src/ATMTest.java
@@ -11,38 +11,38 @@ class ATMTest {
 	public void shouldHaveDefaultBalanceof100() {
 		ATM underTest = new ATM(100, "");
 		BigDecimal balance = underTest.getBalance();
-		assertEquals(100, balance, 0);
+		assertEquals(100, new BigDecimal(100));
 	}
 
 	@Test
 	public void shouldHaveDefaultBalanceOf200() {
 		ATM underTest = new ATM(200, "");
 		BigDecimal balance = underTest.getBalance();
-		assertEquals(200, balance, 0);
+		assertEquals(200, balance);
 	}
 	
 	@Test
 	public void shouldHaveBalanceOf100AfterWithdrawOf100() {
 		ATM underTest = new ATM(200, "");
-		underTest.withdraw(100);
-		double balance = underTest.getBalance();
-		assertEquals(100, balance, 0);
+		underTest.withdraw(new BigDecimal(100));
+		BigDecimal balance = underTest.getBalance();
+		assertEquals(100, new BigDecimal(100));
 	}
 	
 	@Test
 	public void shouldHaveBalanceOf0AfterWithdrawOf300() {
 		ATM underTest = new ATM(200, "");
-		underTest.withdraw(300);
-		double balance = underTest.getBalance();
-		assertEquals(0, balance, 0);
+		underTest.withdraw(new BigDecimal(300));
+		BigDecimal balance = underTest.getBalance();
+		assertEquals(0, new BigDecimal(300));
 	}
 	
 	@Test
 	public void shouldHaveBalanceOf100AfterDeposit() {
 		ATM underTest = new ATM(0, "");
-		underTest.deposit(100);
-		double balance = underTest.getBalance();
-		assertEquals(100, balance, 0);
+		underTest.deposit(new BigDecimal(100));
+		BigDecimal balance = underTest.getBalance();
+		assertEquals(100, new BigDecimal(100));
 	}
 	
 	@Test
