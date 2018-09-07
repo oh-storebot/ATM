19093bb5e724e1534f8f96cb74cf901870cc9459
oh-storebot <eric@negron.nu>
2018-09-07 13:42:58 -0400

    formatting and wrong pin enhancement

--- src/ATMApp.java
+++ src/ATMApp.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 public class ATMApp {
 
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
 		ATM myATM = new ATM(500, "1111");
-		
+
 		System.out.println("Enter your PIN: ");
 		String enteredPin = input.nextLine();
-		
-		if(myATM.getPin().equals(enteredPin)) {
+
+		if (myATM.getPin().equals(enteredPin)) {
 			System.out.println("Welcome to Bank of We Can Code It");
 		}
-		
-		if(!myATM.getPin().equals(enteredPin)) { //only works once.
+
+		while (!myATM.getPin().equals(enteredPin)) { // only works once.
 			System.out.println("Enter your PIN again: ");
-			 enteredPin = input.nextLine();
+			enteredPin = input.nextLine();
 		}
-		
+
 		System.out.println("Choose an option: ");
 		System.out.println("Press 1 to deposit funds");
 		System.out.println("Press 2 to withdraw funds");
@@ -27,24 +27,24 @@ public class ATMApp {
 		System.out.println("Press 4 to exit");
 		String menuOption = input.nextLine();
 
-		while(!menuOption.equals("4")) {
-			if(menuOption.equals("1")) {
+		while (!menuOption.equals("4")) {
+			if (menuOption.equals("1")) {
 				System.out.println("Enter the amount you would like to deposit: ");
 				BigDecimal amountToDeposit = input.nextBigDecimal();
 				myATM.deposit(amountToDeposit);
 				input.nextLine();
-			}else if(menuOption.equals("2")) {
+			} else if (menuOption.equals("2")) {
 				System.out.println("Enter the amount you would like to withdraw: ");
 				BigDecimal amountToWithdraw = input.nextBigDecimal();
 				myATM.withdraw(amountToWithdraw);
 				input.nextLine();
-			}else if (menuOption.equals("3")) {
+			} else if (menuOption.equals("3")) {
 				System.out.println("Your currennt balance is " + myATM.getBalance());
-			}else {
-				System.out.println("Goodbye... Thanks for banking with us today!");
+			} else {
+				System.out.println("Goodbye... Thanks for banking with us today!"); //out of the loop already? That why it skips?
 				System.exit(0);
 			}
-			
+
 			System.out.println("Choose another option: ");
 			System.out.println("Press 1 to deposit funds");
 			System.out.println("Press 2 to withdraw funds");
@@ -52,10 +52,11 @@ public class ATMApp {
 			System.out.println("Press 4 to exit");
 			menuOption = input.nextLine();
 		}
-		System.out.println("Goodbye... Thanks for banking with us today!"); //shouldn't be the way it works but did this way. see like 44
+		System.out.println("Goodbye... Thanks for banking with us today!"); // not sure why line 44 doesn't work had to add this
+																			
 		System.exit(0);
 		input.close();
-		
+
 	}
 
 }
